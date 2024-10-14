1febe0faada1d57c83a026f85b0a825fa275ba38
Floryss Rubechi <floryss.rubechi@gmail.com>
2024-10-14 10:49:59 +0200

    feat: creation of SearchController and its route + deletion of unused .gitignore in /migrations

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * Page witch allows to make a search in recipes with parameters
+     */
+    #[Route('/recherche', name: 'app_search')]
+    public function search(): Response
+    {
+        return $this->render('search/index.html.twig', []);
+    }
+
+    /**
+     * Page witch shows details of a recipe
+     */
+    #[Route('/liker/{id<[0-9]+>}/{like<[0-1]>', name: 'app_recipe')]
+    public function recipeDetails(): Response
+    {
+        return $this->render('search/recipe.html.twig', []);
+    }
+
+    /**
+     * Page witch allows user to like/unlike a recipe
+     */
+    #[Route('/liker/{id<[0-9]+>}/{like<[0-1]>', name: 'app_like')]
+    public function likeRecipe(int $id): Response
+    {
+        return $this->redirectToRoute('app_recipe', ['id' => $id]);
+    }
+}
